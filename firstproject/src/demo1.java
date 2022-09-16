2e70b7b22617edca2787f813b91793b8aaf8b300
User <User@Z422>
2022-09-16 18:56:50 +0530

    first commit

---
+++ firstproject/src/demo1.java
@@ -0,0 +1,13 @@
+
+public class demo1 {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}
+
+
---
+++ firstproject/src/pgm3.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+public class pgm3 {
+	public static void main(String args[]) {
+		Random rand=new Random();
+		int i=rand.nextInt(100);
+		int j=rand.nextInt(100);
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(i>j);
+		
+		
+	}
+
+}
