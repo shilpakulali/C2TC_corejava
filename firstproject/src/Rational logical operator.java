7ec4d8461819760f919bb4b726d56e26a59d2afe
User <User@Z422>
2022-09-16 14:53:56 +0530

    first commit

---
+++ firstproject/src/Rational logical operator.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+public class Rational logical operator {
+
+	public static void main(String[] args) {
+		Random rand=new Random();
+		int i=rand.nextInt(100);
+		System.out.println(i);
+		
+
+	}
+
+}
